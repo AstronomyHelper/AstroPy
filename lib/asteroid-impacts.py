8817150d16338f17bd404393cc463e3e306a59c7
MaxAAustin <maxaustin92@gmail.com>
2018-06-06 13:10:04 -0400

    Added impacts scatterplot

--- lib/asteroid-impacts.py
+++ lib/asteroid-impacts.py
@@ -1,56 +1,28 @@
 # Author: Michael Austin
-# Date: 06/05/2018
-# Data: NASA Asteroid Impacts and Orbits CSVs
-# Purpose Plot impacts and orbits and correlate to develop a prediction service
+# Date: 06/06/2018
+# Data: NASA Asteroid Impacts CSV
+# Purpose Plot impacts and magnitudes; correlate to develop a prediction service
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Data frames of the CSVs
-"""
-Fields for Orbits: Name, Object Classification, Epoch (TDB), Orbit Axis (AU),
-Orbit Eccentricity, Orbit Inclination (deg), Perihelion Argument (deg),
-Node Longitude (deg), Mean Anomoly (deg), Perihelion Distance (AU),
-Aphelion Distance (AU), Orbital Period (yr), Minimum Orbit Intersection Distance (AU), Orbital Reference, Asteroid Magnitude
+dfImpacts = pd.read_csv('data/impacts.csv')
 
+"""
 Fields for impacts.csv: Name, Period Start, Period End, Possible Impacts,
 Cumulative Impact Probability, Asteroid Velocity, Asteroid Magnitude, Asteroid Diameter (km),
 Cumulative Palermo Scale, Maximum Palermo Scale, Maximum Torino Scale 
 """
 
-dfOrbit = pd.read_csv('data/orbits.csv')
-dfImpacts = pd.read_csv('data/impacts.csv')
-
-area = 9
-
-# Max is: 33.2
-max_mag = np.amax(dfOrbit['Asteroid Magnitude'])
-# Min is: 9.45
-min_mag =  np.amin(dfOrbit['Asteroid Magnitude'])
-
-# Max is: 0.7069
-max_OID = np.amax(dfOrbit['Minimum Orbit Intersection Distance (AU)'])
-# Min is: 0.0
-min_OID = np.amin(dfOrbit['Minimum Orbit Intersection Distance (AU)'])
+max_vel= np.amax(dfImpacts['Asteroid Velocity'])
+min_vel= np.amax(dfImpacts['Asteroid Velocity'])
 
 #Prints data head to terminal. 
 #print(dfImpacts.head(10))
-#print()
-#print(dfOrbit.head(10))
 
-"""
-plt.scatter(dfOrbit['Minimum Orbit Intersection Distance (AU)'], dfOrbit['Asteroid Magnitude'], s=area, c='blue', alpha=0.5)
-
-plt.title('Orbital Scatterplot')
+plt.scatter(dfImpacts['Asteroid Velocity'], dfImpacts['Asteroid Magnitude'], s=9, c='red', alpha=0.5)
+plt.title('Asteroid Velocities Compared to Magnitudes')
 plt.xlabel('Magnitude')
-plt.ylabel('Mean Orbital Distance')
-
+plt.ylabel('Velocity')
 plt.show()
-"""
-
-print(max_mag)
-print(min_mag)
-print('---------------------------------------------------------------------------------------')
-print(max_OID)
-print(min_OID)
---
+++ lib/asteroid-orbits.py
@@ -0,0 +1,50 @@
+# Author: Michael Austin
+# Date: 06/05/2018
+# Data: NASA Asteroid Orbits CSV
+# Purpose Plot orbits and correlate to develop a prediction service
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Data frames of the CSVs
+"""
+Fields for Orbits: Name, Object Classification, Epoch (TDB), Orbit Axis (AU),
+Orbit Eccentricity, Orbit Inclination (deg), Perihelion Argument (deg),
+Node Longitude (deg), Mean Anomoly (deg), Perihelion Distance (AU),
+Aphelion Distance (AU), Orbital Period (yr), Minimum Orbit Intersection Distance (AU), Orbital Reference, Asteroid Magnitude
+"""
+
+dfOrbit = pd.read_csv('data/orbits.csv')
+
+# Diameter of the points in scatterplot 
+area = 9
+
+# Max is: 33.2
+max_mag = np.amax(dfOrbit['Asteroid Magnitude'])
+# Min is: 9.45
+min_mag =  np.amin(dfOrbit['Asteroid Magnitude'])
+
+# Max is: 0.7069
+max_OID = np.amax(dfOrbit['Minimum Orbit Intersection Distance (AU)'])
+# Min is: 0.0
+min_OID = np.amin(dfOrbit['Minimum Orbit Intersection Distance (AU)'])
+
+#Prints data head to terminal. 
+#print(dfOrbit.head(10))
+
+"""
+plt.scatter(dfOrbit['Minimum Orbit Intersection Distance (AU)'], dfOrbit['Asteroid Magnitude'], s=area, c='blue', alpha=0.5)
+
+plt.title('Orbital Scatterplot')
+plt.xlabel('Magnitude')
+plt.ylabel('Mean Orbital Distance')
+
+plt.show()
+"""
+
+print(max_mag)
+print(min_mag)
+print('---------------------------------------------------------------------------------------')
+print(max_OID)
+print(min_OID)
