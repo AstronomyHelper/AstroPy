03cf648e08774d2f8c54772e2938f7edf57cc01a
MaxAAustin <maxaustin92@gmail.com>
2018-06-05 22:11:48 -0400

    Made Scatterplot and cleaned up orbits.csv n/a in Mean Orbital Distance

--- lib/asteroid-impacts.py
+++ lib/asteroid-impacts.py
@@ -22,7 +22,27 @@ Cumulative Palermo Scale, Maximum Palermo Scale, Maximum Torino Scale
 dfOrbit = pd.read_csv('data/orbits.csv')
 dfImpacts = pd.read_csv('data/impacts.csv')
 
+area = 9
+
+# Max is: 33.2
+max_mag = np.amax(dfOrbit['Asteroid Magnitude'])
+# Min is: 9.45
+min_mag =  np.amin(dfOrbit['Asteroid Magnitude'])
+
 #Prints data head to terminal. 
-print(dfImpacts.head())
-print()
-print(dfOrbit.head())
+#print(dfImpacts.head(10))
+#print()
+#print(dfOrbit.head(10))
+
+"""
+plt.scatter(dfOrbit['Minimum Orbit Intersection Distance (AU)'], dfOrbit['Asteroid Magnitude'], s=area, c='blue', alpha=0.5)
+
+plt.title('Orbital Scatterplot')
+plt.xlabel('Magnitude')
+plt.ylabel('Mean Orbital Distance')
+
+plt.show()
+"""
+
+print(max_mag)
+print(min_mag)
